4e2eb24853e94ac66914c435bf2471a455ff2477
Alberto Martín Vélez <albertomartinvelez@MacBook-Pro-de-Alberto.local>
2018-03-05 21:28:17 -0600

    lab16 inicio

---
+++ Lab16/index.php
@@ -0,0 +1,10 @@
+<?php
+    session_start();
+    if(isset($_SESSION["nombre"]) ) {
+        header("location: login.php");    
+    } else {
+        include("partials/_header.html");
+        include("partials/_form.html");
+        include("partials/_footer.html");
+    }
+?>
